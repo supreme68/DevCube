62bff55a002cd39c5a6bdfe4eb22427bdaa8f97e
Димитър Арнаудов <dimitararnaudov68@gmail.com>
2018-04-25 00:43:29 +0300

    I HOPE THIS VERSION OF THE DB WORKS

--- DevCube/App_Data/SkilledProgrammerQuerry.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE SkilledProgrammers (
-	SkilledPrgrammesID int IDENTITY(1,1) PRIMARY KEY,
-	SkillsID int FOREIGN KEY REFERENCES Skills(SkillsID),
-	ProgrammersID int FOREIGN KEY REFERENCES Programmers(ProgrammersID),
-)
