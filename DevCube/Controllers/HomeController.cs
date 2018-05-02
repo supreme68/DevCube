2e78ec4c99ec46b1ee4d533dc0e125d73c3ce954
unknown <dimitararnaudov68@gmail.com>
2018-05-02 13:21:08 +0300

    Delete record system ready

---
+++ DevCube/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Web.Mvc;
+using DevCube.Models;
+using DevCube.ViewModels;
+using DevCube.DataAcces;
+namespace DevCube.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public ActionResult Index()
+        {
+            return View(CRUD.IndexGET());
+        }
+
+
+
+        //// GET: Programmers/Create
+        //public ActionResult CreateProgrammer()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult CreateProgrammer(Programmer programmer)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Programmers.Add(programmer);
+        //        db.SaveChanges();
+
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(programmer);
+        //}
+
+        //GET: Programmmers/Update
+
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            return View(CRUD.DeleteGET(id));
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            CRUD.DeletePOST(id);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Update(int? id)
+        {
+          
+            return View(CRUD.UpdateGET(id));
+        }
+
+        [HttpGet]
+        public ActionResult CreateSkill()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateSkill(Skill skill)
+        {
+            if (ModelState.IsValid)
+            {
+                CRUD.CreateSkill(skill);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- DevCube/ViewModels/SkillModel.cs
+++ DevCube/ViewModels/SkillModel.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace DevCube.ViewModels {
-  public class SkillModel {
-    public int SkillID { get; set; }
-    public string Name { get; set; }
-  }
+namespace DevCube.ViewModels
+{
+    public class SkillModel
+    {
+        public int SkillID { get; set; }
+        public string Name { get; set; }
+    }
 }
